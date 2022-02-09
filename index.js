855a835b8b75ea9b918c1bbb2cf3e2837b032876
George <24921711+gliu20@users.noreply.github.com>
2022-02-09 04:42:50 +0000

    add encoder and decoder

---
+++ index.js
@@ -0,0 +1,93 @@
+
+// returns encoding representation
+// where the highest bit represents the i-th bit
+// 1 bits represent using the odd branch
+// 0 bits represent using the even branch
+// the most significant bit (MSB) has no meaning
+// and is present on all encodings (to make sure
+// we always know the number of iterations we need
+// for decoding)
+const encodeCollatz = (bigInt) => {
+    const isEven = (bigInt) => (bigInt & 1n) === 0n;
+    let encoding = 0n, i = 0n;
+
+    while (bigInt !== 1n) {
+        if (isEven(bigInt)) {
+            bigInt >>= 1n; // x / 2
+        }
+        else {
+            const bigIntTimesThree = (bigInt << 1n) + bigInt; // 3 x
+            bigInt = (bigIntTimesThree + 1n) >> 1n; // (3 x + 1) / 2
+
+            // set i-th bit of encoding to 1
+            // this represents that we executed the odd branch
+            // at the i-th position
+            encoding |= (1n << i);
+        }
+        i++;
+    }
+
+    // mark the final i-th bit as set
+    // so that we know the number of iterations
+    return encoding | (1n << i);
+}
+
+const decodeCollatz = (bigInt) => {
+    const bitLengthOf = (bigInt) => {
+        let length = 0n;
+
+        // keep shifting off bits until
+        // we reach the Most Significant Bit
+        // which is always set to 1 as defined
+        // by our encoding
+        while (bigInt !== 1n) {
+            length++;
+            bigInt >>= 1n;
+        }
+
+        return length;
+    };
+
+    const bitAtIndex = (bigInt, index) => { 
+        const bitMask = 1n << index;
+        const bit = bigInt & bitMask; 
+        
+        // right shift to bring bit down to 
+        // a 1 or a 0 instead of a binary
+        // number with lots of trailing 0s
+        return bit >> index;
+    }
+    // collatz conjecture says that all numbers
+    // when iterated by the collatz function
+    // will always reach 1n. this means when we
+    // apply in reverse, all numbers must start
+    // from 1n and be transformed from there.
+    // this is what we will do.
+    let decoding = 1n;
+
+    for (let i = bitLengthOf(bigInt) - 1n; i >= 0n; i--) {
+        // we defined the collatz function as
+        // the modified format
+        // where it is f(x) = {
+        //      x / 2, x is even
+        //      (3x + 1) / 2, x is odd
+        // }
+        // this means that for all branches,
+        // we have to transform the decoding by
+        // multiplying by 2
+
+        decoding <<= 1n; // decoding *= 2
+
+        if (bitAtIndex(bigInt, i) !== 0n) {
+
+            // these instructions are not
+            // optimized with bitwise ops 
+            // b/c there's no way to do so
+            // (decoding - 1) / 3
+            decoding -= 1n;
+            decoding /= 3n;
+        }
+    }
+
+    return decoding;
+}
