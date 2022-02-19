7ce18b5abbe56a991d977f476ce61be0e23d0ccc
George <24921711+gliu20@users.noreply.github.com>
2022-02-19 04:09:35 +0000

    refactor out code for bytes

--- collatz.py
+++ collatz.py
@@ -1,41 +1,13 @@
 """ defines functions for encrypting and decrypting using collatz """
 
-##### utility functions
-
-is_even = lambda num : num & 1 == 0
-
-def get_bit_length(num):
-    """ gets bit length """
-    length = 0
-
-    # guard against infinite loop on invalid args
-    assert num >= 1, "input number must be greater than or equal to 1"
-
-    # the MSB must be 1 because we are working
-    # with arbitrarily large integers; once we
-    # reach 1, we know to stop
-    while num != 1:
-        length += 1
-        num >>= 1
-
-    return length
-
-def get_bit_at_index(num, index):
-    """ gets a single bit at the specified index from num """
-    bit_mask = 1 << index
-    bit = num & bit_mask
-
-    # we have to right shift to the bit to get rid of all
-    # the trailing zeros
-    return bit >> index
-
-##### main functions
+from bytes_int import is_even, get_bit_at_index, get_bit_length
 
 # we define the collatz function as
 # f(x) = {
 #   x / 2, x is even
 #   (3x + 1) / 2, x is odd
 # }
+
 
 def encode_collatz(num):
     """ takes num and encodes it into collatz (also a num)"""
@@ -47,8 +19,8 @@ def encode_collatz(num):
             # x / 2
             num >>= 1
         else:
-            num_times_three = (num << 1) + num # 3x
-            num = (num_times_three + 1) >> 1 # (3x + 1) / 2
+            num_times_three = (num << 1) + num  # 3x
+            num = (num_times_three + 1) >> 1  # (3x + 1) / 2
 
             # set i-th bit to 1
             encoding |= (1 << i)
@@ -57,6 +29,7 @@ def encode_collatz(num):
     # we mark the final (MSB) i-th bit as 1 so we know
     # the total amount of iterations for decoding
     return encoding | (1 << i)
+
 
 def decode_collatz(num):
     """ decode collatz encoding"""
@@ -76,6 +49,6 @@ def decode_collatz(num):
             # reverse the odd branch of the collatz function
             # decoding = (decoding - 1) / 3
             decoding -= 1
-            decoding //= 3 # divide as ints
+            decoding //= 3  # divide as ints
 
     return decoding
