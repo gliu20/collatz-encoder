ecfff113e318954ebb9293f62123b35a4b158bfd
gliu20 <24921711+gliu20@users.noreply.github.com>
2024-07-04 18:09:26 -0400

    fix compiler warning

--- include/limb_math_radix_pow2.h
+++ include/limb_math_radix_pow2.h
@@ -3,4 +3,4 @@
 #include "limb_list.h"
 
 void set_ith_bit(limb_vec_t* ll, size_t bit_index);
-void get_ith_bit(limb_vec_t* ll, size_t bit_index);
+limb_t get_ith_bit(limb_vec_t* ll, size_t bit_index);
