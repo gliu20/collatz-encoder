d44ad10ce002661188d837da687f40dbe6a95db2
gliu20 <24921711+gliu20@users.noreply.github.com>
2024-07-04 18:05:53 -0400

    port utilities for bit setting

--- include/limb_math_radix_pow2.h
+++ include/limb_math_radix_pow2.h
@@ -1,1 +1,6 @@
 #pragma once
+
+#include "limb_list.h"
+
+void set_ith_bit(limb_vec_t* ll, size_t bit_index);
+void get_ith_bit(limb_vec_t* ll, size_t bit_index);
---
+++ src/limb_math_radix_pow2.c
@@ -0,0 +1,19 @@
+#include "limb.h"
+#include "limb_math_common.h"
+#include "limb_math_radix_pow2.h"
+
+void set_ith_bit(limb_vec_t* ll, size_t bit_index) {
+    size_t desired_limb = bit_index / LIMB_CONTAINER_BIT_LENGTH;
+    size_t desired_bit = bit_index % LIMB_CONTAINER_BIT_LENGTH;
+    
+    pad_to_length(ll, desired_limb + 1);
+    
+    LL_INDEX(ll, desired_limb) |= ((limb_t) 1) << desired_bit;
+}
+
+limb_t get_ith_bit(limb_vec_t* ll, size_t bit_index) {
+    size_t desired_limb = bit_index / LIMB_CONTAINER_BIT_LENGTH;
+    size_t desired_bit = bit_index % LIMB_CONTAINER_BIT_LENGTH;
+    
+    return LL_INDEX(ll, desired_limb) & (((limb_t) 1) << desired_bit);
+}
